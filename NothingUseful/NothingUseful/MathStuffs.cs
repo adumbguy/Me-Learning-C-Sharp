b00f7fb5a1d53b9d6b3f31e37278470e0b79c0c2
adumbguy <adumbguyu812@gmail.com>
2014-09-19 11:19:34 -0400

    Math 101
    Added a small math class, gets user input to assign to variables, and
    only adds and subtracts as of right now. Done working on it for now,
    will continue tonight at some point

---
+++ NothingUseful/NothingUseful/MathStuffs.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace NothingUseful
+{
+	public class MathStuffs
+	{
+		public static void Stuff ()
+		{
+			Console.SetWindowSize (400, 400);
+			Console.WriteLine ();
+			Console.WriteLine ("Hello and welcome to MathStuffs!");
+			Console.WriteLine ("It's alot like what it sounds, lets get started!");
+
+			Console.WriteLine ("First, adding. Enter a number for x and y when prompted.");
+
+			int x1 = 0;
+			int y1 = 0;
+			bool valid = false;
+
+			while (valid == false) {
+				Console.Write ("Please enter a number for X : ");
+				if (int.TryParse (Console.ReadLine (), out x1)) {
+					valid = true;
+				} else {
+					Console.WriteLine ("Please enter a valid number.");
+				}
+			}
+
+			valid = false;
+
+			while (valid == false) {
+				Console.Write ("Now enter a number for Y : ");
+				if (int.TryParse (Console.ReadLine (), out y1)) {
+					valid = true;
+				}else{
+					Console.WriteLine ("Please enter a valid number.");
+				}
+			}
+				int z1 = x1 + y1;
+			
+			Console.WriteLine ("Okay, so " + x1.ToString() + " + " + y1.ToString() + " = " + z1.ToString());
+
+			Console.WriteLine ();
+			Console.Write ("Awesome! Now press ENTER to go onto subtracting!");
+			Console.ReadLine ();
+			Console.WriteLine ();
+
+			valid = false;
+			int x2 = 0;
+			int y2 = 0;
+
+			Console.WriteLine ("Let's do that same thing as adding, enter a value for x and y.");
+			while(valid == false){
+				Console.Write ("Enter an X value : ");
+				if(int.TryParse(Console.ReadLine(), out x2)){
+					valid = true;
+				}else{
+					Console.WriteLine ("Please enter a valid number.");
+				}
+			}
+
+			valid = false;
+
+			while (valid == false) {
+				Console.Write ("Now a value for Y : ");
+				if (int.TryParse (Console.ReadLine (), out y2)) {
+					valid = true;
+				} else {
+					Console.WriteLine ("Please enter a valid number.");
+				}
+			}
+			int z2 = x2 - y2;
+			Console.WriteLine("And " + x2.ToString() + " - " + y2.ToString() + " = " +z2.ToString());
+
+			Console.Write ("END");
+			Console.Read();
+		}
+	}
+}
+
--- NothingUseful/NothingUseful/Program.cs
+++ NothingUseful/NothingUseful/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using NothingUseful;
 
 namespace NothingUseful
 {
@@ -6,7 +7,15 @@ namespace NothingUseful
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("Welcome! Here is my useless program 2.0!");
+			Console.WriteLine ("It's only 2.0 because it's writen in C# instead of Java.");
+			Console.WriteLine ("Anyways, I planned on saving most of my learning in this project!");
+			Console.WriteLine ("Oh, and don't expect it to look pretty either, I'm not super awesome at this yet.");
+			Console.WriteLine ("");
+			Console.Write ("Press ENTER to get on with it...");
+			Console.ReadLine();
+
+			MathStuffs.Stuff ();
 		}
 	}
 }
